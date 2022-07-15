85af84fd267021d0d43e8faf904008f3ed32a7ce
Code With Eras <82660915+codewitheras@users.noreply.github.com>
2022-07-14 21:33:03 -0700

    updated the firebase config file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,39 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth';
 
+// Updated the Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCVsbXJVgfW4GmDJrICgJsiKVl3--dKNHY",
-  authDomain: "courselab-db.firebaseapp.com",
-  projectId: "courselab-db",
-  storageBucket: "courselab-db.appspot.com",
-  messagingSenderId: "480384435172",
-  appId: "1:480384435172:web:c3f4b71051fc14fe3dbdf1",
-  measurementId: "G-YHFDX2BE8N",
+  apiKey: "AIzaSyDg44ztuhLsOwwDXfGAXQrCVAliS6u-Bk0",
+  authDomain: "codewitheras-platform.firebaseapp.com",
+  projectId: "codewitheras-platform",
+  storageBucket: "codewitheras-platform.appspot.com",
+  messagingSenderId: "910632721930",
+  appId: "1:910632721930:web:d29e7db2a09826483489f3"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+// Passwordless Sign In
+
+/*
+
+import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
+
+const auth = getAuth();
+sendSignInLinkToEmail(auth, email, actionCodeSettings)
+  .then(() => {
+    // The link was successfully sent. Inform the user.
+    // Save the email locally so you don't need to ask the user for it again
+    // if they open the link on the same device.
+    window.localStorage.setItem('emailForSignIn', email);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+
+*/
