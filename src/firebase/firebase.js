da2ac5453211ef84b92f4c1f5c4c612eba21a453
Code With Eras <82660915+codewitheras@users.noreply.github.com>
2022-07-14 21:47:41 -0700

    minor

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink } from 'firebase/auth';
+import { getAuth} from 'firebase/auth';
 
 // Updated the Config
 const firebaseConfig = {
